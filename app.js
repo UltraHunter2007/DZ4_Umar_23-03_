9ab6c07d9a1dfb813b19d447b10a77e0a79575ea
UltraHunter2007 <abdilaipovumar2007@gmail.com>
2023-03-24 08:32:32 +0600

    one function

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const btn1 = document.querySelector('.btn1')
-const btn2 = document.querySelector('.btn2')
-const btn3 = document.querySelector('.btn3')
+const btn = document.querySelector('.btn')
 
-function loadData(button, i, imageSrc, info) {
+btn.addEventListener('click', () => {
     const request = new XMLHttpRequest()
     request.open("GET", "people.json")
     request.setRequestHeader("Content-type", "application/json")
     request.send()
     request.addEventListener('load', () => {
         const data = JSON.parse(request.response)
-        document.querySelector(`.name${info}`).innerHTML = data[i].name
-        document.querySelector(`.age${info}`).innerHTML = data[i].age
-        const img = document.querySelector(`.photo${info}`)
-        img.src = imageSrc
-        img.alt = data[i].name
-        img.style.display = 'block'
+        document.querySelector('.name1').innerHTML = data[0].name
+        document.querySelector('.age1').innerHTML = data[0].age
+        const img1 = document.querySelector('.photo1')
+        img1.src = 'images/John.jpg'
+        img1.alt = data[0].name
+        img1.style.display = 'block'
+
+        document.querySelector('.name2').innerHTML = data[1].name
+        document.querySelector('.age2').innerHTML = data[1].age
+        const img2 = document.querySelector('.photo2')
+        img2.src = 'images/Steve.jpg'
+        img2.alt = data[1].name
+        img2.style.display = 'block'
+
+        document.querySelector('.name3').innerHTML = data[2].name
+        document.querySelector('.age3').innerHTML = data[2].age
+        const img3 = document.querySelector('.photo3')
+        img3.src = 'images/Light.jpg'
+        img3.alt = data[2].name
+        img3.style.display = 'block'
     })
-}
-
-btn1.addEventListener('click', () => {
-    loadData(btn1, 0, "images/John.jpg", 1)
-})
-
-btn2.addEventListener('click', () => {
-    loadData(btn2, 1, "images/Steve.jpg", 2)
-})
-
-btn3.addEventListener('click', () => {
-    loadData(btn3, 2, "images/Light.jpg", 3)
 })
